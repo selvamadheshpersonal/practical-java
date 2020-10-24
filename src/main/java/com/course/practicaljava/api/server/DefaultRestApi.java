34c951e04a94c9daccf494d575345a640a66323e
SELVA KUMAR ANANTHARAJ <selvakumara@fss.co.in>
2020-10-24 15:04:16 +0530

    Add log to DefaultRestApi
    Chaned sysout to logger

--- src/main/java/com/course/practicaljava/api/server/DefaultRestApi.java
+++ src/main/java/com/course/practicaljava/api/server/DefaultRestApi.java
@@ -3,6 +3,8 @@
  */
 package com.course.practicaljava.api.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DefaultRestApi {
 
+	Logger LOGGER = LoggerFactory.getLogger(DefaultRestApi.class);
+
 	@RequestMapping(value = "/welcome")
 	public String welcome() {
+		LOGGER.debug("Welcome");
 		return "Welcome to Spring Boot";
 	}
 }
